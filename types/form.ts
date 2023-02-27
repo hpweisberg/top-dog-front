78cf35de04f7caf54c855c90f818fc0b1c1e2d62
hpweisberg <hpweisberg@gmail.com>
2023-02-27 14:12:47 -0800

    add auth forms

---
+++ types/form.ts
@@ -0,0 +1,25 @@
+
+
+// ---------- Auth Forms -----------
+
+export interface LoginFormData {
+  email: string;
+  password: string;
+}
+
+export interface SignupFormData {
+  name: string;
+  email: string;
+  password: string;
+  passwordConf: string;
+}
+
+export interface ChangePasswordFormData {
+  oldPassword: string;
+  newPassword: string;
+  newPasswordConf: string;
+}
+
+export interface PhotoFormData {
+  photo: File | null;
+}
