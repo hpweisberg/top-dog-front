c9bc9907e7ef63524cf37e966979371eeef4d693
hpweisberg <hpweisberg@gmail.com>
2023-02-27 19:26:51 -0800

    add onclick function

--- src/components/VoteManager/VoteManager.tsx
+++ src/components/VoteManager/VoteManager.tsx
@@ -4,14 +4,16 @@ import noBone from '../../assets/icons/noBean.png'
 
 // types
 import { Profile } from '../../types/models'
+import { VoteManagerFormData } from '../../types/forms'
 
 interface VoteManagerProps {
   profile: Profile;
+  handleVote: (formData: VoteManagerFormData) => void;
 }
 
 
 const VoteManager = (props: VoteManagerProps): JSX.Element => {
-  const { profile } = props
+  const { profile, handleVote } = props
   
   const ratingOptions: [1, 2, 3, 4, 5] = [1, 2, 3, 4, 5]
   const voteCount = profile.votesReceived.length
@@ -21,12 +23,18 @@ const VoteManager = (props: VoteManagerProps): JSX.Element => {
 
   const profileRating = voteCount ? voteSum / voteCount : 1
 
+  const handleClick = (evt: React.MouseEvent<HTMLImageElement>): void => {
+    const newValue = parseInt(evt.currentTarget.id)
+    handleVote({ value: newValue, profileId: profile.id })
+  }
+
   return ( 
     <section>
       {ratingOptions.map((rating: number): JSX.Element => (
         <img
           id={rating.toString()}
           key={rating}
+          onClick={handleClick}
           src={rating <= profileRating ? bone : noBone}
           alt='Bone Symbol'
         />
