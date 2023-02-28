4a82396b35b84efb20419626e86a1e0a33dc0116
hpweisberg <hpweisberg@gmail.com>
2023-02-27 16:51:13 -0800

    add profile elements to card

--- src/components/ProfileCard/ProfileCard.tsx
+++ src/components/ProfileCard/ProfileCard.tsx
@@ -11,11 +11,13 @@ interface ProfileCardProps {
 const ProfileCard = (props: ProfileCardProps): JSX.Element => {
   const { profile } = props
 
-  
+  const profilePic = profile.photo ? profile.photo : defaultPic
 
   return ( 
-    <>
-    </>
+    <div>
+      <img src={profilePic} alt={`${profile.name}'s avatar`} />
+      <h1>{profile.name}</h1>
+    </div>
   )
 }
 
