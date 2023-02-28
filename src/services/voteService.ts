271aa391a23af702aa6ad2417b61db1fa7c016cf
hpweisberg <hpweisberg@gmail.com>
2023-02-27 18:21:05 -0800

    add voteManagerFormData to castVote vunction

--- src/services/voteService.ts
+++ src/services/voteService.ts
@@ -3,10 +3,11 @@ import * as tokenService from './tokenService'
 
 // types
 import { Profile } from '../types/models'
+import { VoteManagerFormData } from '../types/forms'
 
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/api/votes`
 
-async function castVote(formData): Promise<Profile> {
+async function castVote(formData: VoteManagerFormData): Promise<Profile> {
   try {
     const res = await fetch(BASE_URL, {
       method: 'PUT',
@@ -16,7 +17,7 @@ async function castVote(formData): Promise<Profile> {
       },
       body: JSON.stringify(formData)
     })
-    return await res.json()
+    return await res.json() as Profile
   } catch (error) {
     throw error
   }
