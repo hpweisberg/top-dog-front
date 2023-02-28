95ecec806c453e7ce940bf2dd9de7dc1bfd9c0cf
hpweisberg <hpweisberg@gmail.com>
2023-02-27 16:47:46 -0800

    stub up ProfileCard component

---
+++ src/components/ProfileCard/ProfileCard.tsx
@@ -0,0 +1,22 @@
+// assets
+import defaultPic from '../../assets/icons/profile.png'
+
+// types
+import { Profile } from '../../types/models'
+
+interface ProfileCardProps {
+  profile: Profile;
+}
+
+const ProfileCard = (props: ProfileCardProps): JSX.Element => {
+  const { profile } = props
+
+  
+
+  return ( 
+    <>
+    </>
+  )
+}
+
+export default ProfileCard
